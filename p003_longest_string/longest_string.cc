0d769695ed49ab845cb9b2c54c596583c1e5ec50
Hao Wang <pkuwangh@gmail.com>
2016-09-08 23:19:53 -0500

    [003] longest substring without duplicate character; sliding window

---
+++ p003_longest_string/longest_string.cc
@@ -0,0 +1,54 @@
+/* Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3.
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string s) {
+        size_t begin = 0;
+        size_t end = 0;
+        int max_length = 0;
+        vector<bool> exist(256, false);
+        while (end < s.length()) {
+            if (!exist[s[end]]) {
+                exist[s[end]] = true;
+                ++end;
+                int length = end - begin;
+                if (length > max_length) {
+                    max_length = length;
+                }
+            } else {
+                while (exist[s[end]]) {
+                    exist[s[begin]] = false;
+                    ++begin;
+                }
+            }
+        }
+        return max_length;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+//    string str = "pwwkew";
+//    string str = "bbbbb";
+    string str = "abcabcbb";
+    cout << str << endl;
+    cout << obj.lengthOfLongestSubstring(str);
+    cout << endl;
+
+    return 0;
+}
+
