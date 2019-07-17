1ee92844c3c5322327f78e5a499a3a0d59c8150f
pkuwangh <pkuwangh@gmail.com>
2019-07-17 01:51:05 -0500

    [224] basic calculator; stack

--- p224_basic_calculator/basic_calculator.cc
+++ p224_basic_calculator/basic_calculator.cc
@@ -15,10 +15,70 @@
  * Do not use the eval built-in library function.
 */
 
+#include <vector>
+#include <stack>
+#include <cassert>
+#include <utility>
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Solution {
   public:
     int calculate(string s) {
-
+        int idx = 0;
+        int ans = 0;
+        int sign = 1;
+        while (idx < s.length()) {
+            if (s[idx] == ' ') {
+                // skip
+            } else if (s[idx] >= '0' && s[idx] <= '9') {
+                int num = 0;
+                while (idx < s.length() && s[idx] >= '0' && s[idx] <= '9') {
+                    num = 10*num + (s[idx] - '0');
+                    ++ idx;
+                }
+                ans += sign * num;
+                idx -= 1;
+            } else if (s[idx] == '+') {
+                sign = 1;
+            } else if (s[idx] == '-') {
+                sign = -1;
+            } else if (s[idx] == '(') {
+                // push into stack
+                ops.push(make_pair(ans, sign));
+                ans = 0;
+                sign = 1;
+            } else if (s[idx] == ')') {
+                // pop from stack
+                auto item = ops.top();
+                ans = item.first + item.second * ans;
+                sign = 1;
+                ops.pop();
+            } else {
+                cout << s[idx] << endl;
+                assert(0);
+            }
+            //cout << "idx=" << idx << " ans=" << ans << endl;
+            idx += 1;
+        }
+        return ans;
     }
+
+  private:
+    stack<pair<int, int>> ops;
 };
 
+int main() {
+    Solution obj;
+    auto run = [&](string s)->void {
+        cout << s << " = " << obj.calculate(s) << endl;
+    };
+    run("5 - (6 - 3)");
+    run("1 + 1");
+    run(" 2-1 + 2 ");
+    run(" 2-14 + 2 ");
+    run("(1+(4+5+2)-3)+(6+8)");
+    run("2147483647");
+    return 0;
+}
