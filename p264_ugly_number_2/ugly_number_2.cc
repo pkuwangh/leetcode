54998fdae0dedd313e1e453cbe29259c0945480b
Hao Wang <pkuwangh@gmail.com>
2019-10-05 00:09:08 -0500

    [263] ugly number; simple math

---
+++ p264_ugly_number_2/ugly_number_2.cc
@@ -0,0 +1,43 @@
+/**
+ * Write a program to find the n-th ugly number.
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+ * Example:
+ * Input: n = 10
+ * Output: 12
+ * Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+ * Note:  
+ * 1 is typically treated as an ugly number.
+ * n does not exceed 1690.
+ */
+
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <list>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <functional>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+class Solution {
+  public:
+    int nthUglyNumber(int n) {
+
+    }
+};
+
+int main() {
+    Solution obj;
+    auto run = [&]() ->void {
+    };
+    return 0;
+}
