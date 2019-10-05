36e52ef7b8bf61170663937ac723c12cfec90f26
Hao Wang <pkuwangh@gmail.com>
2019-10-05 11:55:55 -0500

    [264] ugly number 2; pruning, probably there is better sol

--- p264_ugly_number_2/ugly_number_2.cc
+++ p264_ugly_number_2/ugly_number_2.cc
@@ -30,14 +30,48 @@ using namespace std;
 
 class Solution {
   public:
-    int nthUglyNumber(int n) {
+    bool checkNum(const int& num, const unordered_set<int>& ugly_nums) {
+        if (num == 1) return true;
+        if (num % 2 == 0 && ugly_nums.count(num/2) > 0) return true;
+        if (num % 3 == 0 && ugly_nums.count(num/3) > 0) return true;
+        if (num % 5 == 0 && ugly_nums.count(num/5) > 0) return true;
+        return false;
+    }
 
+    int nthUglyNumber(int n) {
+        set<uint64_t, less<uint64_t>> ugly_seeds;
+        ugly_seeds.insert(1);
+        uint64_t num = 0;
+        for (int i = 1; i <= n; ++i) {
+            num = *(ugly_seeds.begin());
+            // push into new seeds
+            ugly_seeds.insert(num*2);
+            ugly_seeds.insert(num*3);
+            ugly_seeds.insert(num*5);
+            ugly_seeds.erase(ugly_seeds.begin());
+        }
+        return num;
+
+//        unordered_set<int> ugly_nums;
+//        int num = 0;
+//        for (int i = 0; i < n; ++i) {
+//            do {
+//                ++ num;
+//            } while (!checkNum(num, ugly_nums));
+//            ugly_nums.insert(num);
+//        }
+//        return num;
     }
 };
 
 int main() {
     Solution obj;
-    auto run = [&]() ->void {
+    auto run = [&](int n) ->void {
+        cout << n << " -> " << obj.nthUglyNumber(n) << endl;
     };
+    run(1);
+    run(10);
+    run(800);
+    run(1689);
     return 0;
 }
